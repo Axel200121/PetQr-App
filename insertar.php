de58013480e37a32ddb175310bc869dcc90035a9
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-10 18:24:58 -0500

    Se creo Método insertar a usuarios
    Se programo la función la inserción de usuarios

---
+++ insertar.php
@@ -0,0 +1,18 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    require_once 'conexion.php';
+    $nombre=$_POST["nombre"];
+    $apellidos=$_POST["apellidos"];
+    $telefono=$_POST["telefono"];
+    $direccion=$_POST["direccion"];
+    $correo=$_POST["correo"];
+    $password=$_POST["password"];
+    $query="INSERT INTO usuarios (nombre,apellidos,telefono,direccion,correo,password) 
+    VALUES('".$nombre."','".$apellidos."','".$telefono."','".$direccion."','".$correo."','".$password."')";
+    $resultado=$mysql->query($query);
+    if($resultado==true){
+        echo "El usuario se inserto de forma exitosa";
+    }else{
+        echo "Error al insertar el usuario";
+    }
+}
