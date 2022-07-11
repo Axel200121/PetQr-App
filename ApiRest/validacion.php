1634b0088b8461d87c11ac5765b79d2d81a49193
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-11 14:12:25 -0500

    Metodo Validar
    Para la validación de los de los inicio de sesión

--- ApiRest/validacion.php
+++ ApiRest/validacion.php
@@ -1,1 +1,12 @@
 <?php
+function validate($data) {
+    // Elimine los caracteres innecesarios, por ejemplo, espacio adicional, tabulador, nueva línea de la entrada del usuario
+    $data = trim($data);
+    // Eliminar barras invertidas
+    $data = stripslashes($data);
+    // Convierta caracteres especiales en entidades HTML, evitando así que los atacantes exploten el código
+    $data = htmlspecialchars($data);
+    // Devolver datos validados
+    return $data;
+}
+?>
