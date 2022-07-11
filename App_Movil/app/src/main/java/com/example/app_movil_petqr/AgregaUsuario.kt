0d0f24c459fd3b120d71f44b5a10b5242d1a94c3
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-10 21:21:32 -0500

    Agregación de la dependencia Volley

---
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/AgregaUsuario.kt
@@ -0,0 +1,30 @@
+package com.example.app_movil_petqr
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class AgregaUsuario : AppCompatActivity() {
+    var txt_nombre:EditText?=null;
+    var txt_apellidos:EditText?=null;
+    var txt_telefono:EditText?=null;
+    var txt_direccion:EditText?=null;
+    var txt_correo:EditText?=null;
+    var txt_password:EditText?=null;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_usuario)
+
+        txt_nombre = findViewById(R.id.txt_nombre)
+        txt_apellidos = findViewById(R.id.txt_apellidos)
+        txt_telefono = findViewById(R.id.txt_telefono)
+        txt_direccion = findViewById(R.id.txt_telefono)
+        txt_correo = findViewById(R.id.txt_correo)
+        txt_password = findViewById(R.id.txt_password)
+    }
+
+    
+}
