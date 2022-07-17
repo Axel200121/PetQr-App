d0b15875f4be6311d4a1e7726b459c18f0bd3508
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-17 00:12:45 -0500

    Creación del servicio consultar mascota

---
+++ ApiRest/mascotas/MascotaConsultar.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"]=="GET"){
+    require_once "../conexion.php";
+    $idUsuario = $_GET['idUsuario'];
+    $query = "SELECT mascota.nombre,tipoMascota,descripcion 
+                FROM mascota INNER JOIN usuario ON mascota.idUsuario=usuario.idUsuario 
+                WHERE usuario.idUsuario ='".$idUsuario."'";
+    $resultado = $mysql->query($query);
+    if ($mysql->affected_rows > 0){
+        //va jalando cada uno de los registro
+        while ($row=$resultado->fetch_assoc()){
+            $array = $row;
+        }
+        //nos devuelve la informacion en JSON
+        echo json_encode($array);
+    }else{
+        echo "Mascotas no encontrado";
+    }
+}
