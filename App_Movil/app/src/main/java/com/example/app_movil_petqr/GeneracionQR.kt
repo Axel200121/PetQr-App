28ae8fb6662981ff8181ba450036e4907548cbf0
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-18 20:37:37 -0500

    Agregación UI de la vista panel de las mascotas

--- App_Movil/app/src/main/java/com/example/app_movil_petqr/GeneracionQR.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/GeneracionQR.kt
@@ -1,16 +1,11 @@
 package com.example.app_movil_petqr
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 
-class Home : AppCompatActivity() {
+class GeneracionQR : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
-
+        setContentView(R.layout.activity_generacion_qr)
     }
 }
--- App_Movil/app/src/main/java/com/example/app_movil_petqr/PanelMascota.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/PanelMascota.kt
@@ -1,16 +1,11 @@
 package com.example.app_movil_petqr
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 
-class Home : AppCompatActivity() {
+class PanelMascota : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-
-
+        setContentView(R.layout.activity_panel_mascota)
     }
 }
