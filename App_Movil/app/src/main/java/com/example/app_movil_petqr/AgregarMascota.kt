076860c1d25d0a538070aa1f15c4faf7c0c520b4
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-18 23:12:46 -0500

    consumiendo el servicio agregar mascota

--- App_Movil/app/src/main/java/com/example/app_movil_petqr/AgregarMascota.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/AgregarMascota.kt
@@ -2,10 +2,44 @@ package com.example.app_movil_petqr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
-class AgregarMascota : AppCompatActivity() {
+class AgregarMascota : AppCompatActivity(){
+    var txtNombreMascota:EditText?=null
+    var txtTipoMascota:EditText?=null
+    var txtDescripcionMascota:EditText?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_mascota)
+
+        txtNombreMascota = findViewById(R.id.txtNombreMascota)
+        txtTipoMascota = findViewById(R.id.txtTipoMascota)
+        txtDescripcionMascota= findViewById(R.id.txtDescripcionMascota)
+
+    }
+    fun clickBtnInsertarMascota(view: View){
+        val url= "http://192.168.100.71/PetQr-App/ApiRest/mascotas/MascotaInsertar.php"
+        val queue=Volley.newRequestQueue(this)
+        var resultadoPostMascota = object : StringRequest(Method.POST,url,
+            Response.Listener { response ->
+                Toast.makeText(this,"Mascota registrada exitosamente",Toast.LENGTH_LONG).show()
+            },Response.ErrorListener { error ->
+                Toast.makeText(this,"Error al registra tu mascota",Toast.LENGTH_LONG).show()
+            }){
+            override fun getParams(): MutableMap<String, String>? {
+                val parametros=HashMap<String,String>()
+                parametros.put("nombre",txtNombreMascota?.text.toString())
+                parametros.put("tipoMascota",txtTipoMascota?.text.toString())
+                parametros.put("descripcion",txtDescripcionMascota?.text.toString())
+                parametros.put("idUsuario","1")
+                return parametros
+            }
+        }
+        queue.add(resultadoPostMascota)
     }
 }
