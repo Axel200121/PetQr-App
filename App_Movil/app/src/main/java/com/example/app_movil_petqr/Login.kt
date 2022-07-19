796f34e516a8e0583cbffa54d3205ab1631b646b
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-18 19:37:25 -0500

    Consumiendo el servicio para logearse

--- App_Movil/app/src/main/java/com/example/app_movil_petqr/Login.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/Login.kt
@@ -3,17 +3,63 @@ package com.example.app_movil_petqr
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class Login : AppCompatActivity() {
+    var txtCorreoLogin: EditText?=null;
+    var txtPasswordLogin: EditText?=null;
+    var btnIngresarLogin: Button?=null;
+    var btnRegistrarFormulario: Button?=null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val btnRegistraFormulario = findViewById<Button>(R.id.btnRegistrarFormulario)
-        btnRegistraFormulario.setOnClickListener {
-            val lanzarRegistroFormulario = Intent(this,AgregaUsuario::class.java)
-            startActivity(lanzarRegistroFormulario)
+        txtCorreoLogin = findViewById(R.id.txtCorreoLogin)
+        txtPasswordLogin = findViewById(R.id.txtPasswordLogin)
+        btnIngresarLogin = findViewById(R.id.btnIngresarLogin)
+        btnRegistrarFormulario = findViewById(R.id.btnRegistrarFormulario)
+
+        btnRegistrarFormulario!!.setOnClickListener {
+            val lanzar = Intent(this,AgregaUsuario::class.java)
+            startActivity(lanzar)
         }
+
+        btnIngresarLogin!!.setOnClickListener {
+            validarUsuario("http://192.168.100.71/PetQr-App/ApiRest/usuarios/UsuarioLogin.php")
+        }
+    }
+    private fun validarUsuario(URL: String) {
+        val stringRequest: StringRequest = object : StringRequest(
+            Method.POST,
+            URL,
+            Response.Listener { response ->
+                if(!response.isEmpty()){
+                    val intent = Intent(applicationContext, Home::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login, "Usuario o contraseña incorrecto", Toast.LENGTH_SHORT).show()
+                }
+            },
+            Response.ErrorListener { error ->
+                Toast.makeText(this@Login, error.toString(), Toast.LENGTH_SHORT).show()
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val parametros: MutableMap<String, String> = HashMap()
+                parametros.put("correo",txtCorreoLogin?.text.toString())
+                parametros.put("psw",txtPasswordLogin?.text.toString())
+                return parametros
+            }
+        }
+        val requestQueue = Volley.newRequestQueue(this)
+        requestQueue.add(stringRequest)
     }
 }
