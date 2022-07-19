6f4da26bf1e9a69b647f4361da23d265e29946df
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-18 20:26:05 -0500

    Enrutamiento de la vista Home

--- App_Movil/app/src/main/java/com/example/app_movil_petqr/Home.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/Home.kt
@@ -1,11 +1,16 @@
 package com.example.app_movil_petqr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        
     }
 }
