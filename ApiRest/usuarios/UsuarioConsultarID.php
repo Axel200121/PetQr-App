a162eb9b65716abe42428e194d7445dc6048126b
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-25 15:50:55 -0500

    Creacion del servicio consultar por ID

---
+++ ApiRest/usuarios/UsuarioConsultarID.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"]=="GET"){
+    require_once "../conexion.php";
+    $idUsuario = $_GET['idUsuario'];
+    //hacemos la consulta para traer el registro
+    $query = "SELECT * FROM usuario WHERE idUsuario='".$idUsuario."'";
+    $resultado = $mysql->query($query);
+    if ($mysql->affected_rows > 0){
+        //va jalando cada uno de los registro
+        while ($row=$resultado->fetch_assoc()){
+            $array = $row;
+        }
+        //nos devuelve la informacion en JSON
+        echo json_encode($array);
+    }else{
+        echo "Usuario no encontrado";
+    }
+}
