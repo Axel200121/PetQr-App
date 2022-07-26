f1ee629d449ba490c95c2e86b6fb00e90ffeb3cd
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-25 21:46:54 -0500

    Creaando la pantalla editar mascota

---
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/EditarMascota.kt
@@ -0,0 +1,11 @@
+package com.example.app_movil_petqr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditarMascota : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_mascota)
+    }
+}
