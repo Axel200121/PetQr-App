6a0a27f6d553d0ab2622def19c05834e3b5fc068
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-25 22:22:22 -0500

    Consumiendo el servicio actualizar mascota

--- App_Movil/app/src/main/java/com/example/app_movil_petqr/EditarMascota.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/EditarMascota.kt
@@ -2,12 +2,14 @@ package com.example.app_movil_petqr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 
 class EditarMascota : AppCompatActivity() {
@@ -16,6 +18,7 @@ class EditarMascota : AppCompatActivity() {
     var txtDescripcionMascotaEditar: EditText?=null
     var tvIdMascotaEditar: TextView?=null
     var idMascota:String?=null
+    var idUsuario:String?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editar_mascota)
@@ -26,6 +29,8 @@ class EditarMascota : AppCompatActivity() {
 
         idMascota=intent.getStringExtra("idMascota").toString()
         tvIdMascotaEditar?.setText(idMascota)
+
+        idUsuario=intent.getStringExtra("idUsuario").toString()
 
         val queue= Volley.newRequestQueue(this)
         val url="http://192.168.8.101/PetQr-App/ApiRest/mascotas/MascotaConsultar.php?idMascota=$idMascota"
@@ -40,5 +45,28 @@ class EditarMascota : AppCompatActivity() {
             }
         )
         queue.add(jsonObjectRequest)
+    }
+    fun editarFinalMascota(view:View){
+        val url="http://192.168.8.101/PetQr-App/ApiRest/mascotas/MascotaEditar.php"
+        val queue=Volley.newRequestQueue(this)
+        val resultadoPost = object : StringRequest(Request.Method.POST,url,
+            Response.Listener { response ->
+                Toast.makeText(this,"Datos de la mascota actualizados",Toast.LENGTH_LONG).show()
+
+            },Response.ErrorListener { error ->
+                Toast.makeText(this,"Error al modificar datos de la mascota $error",Toast.LENGTH_LONG).show()
+            }
+        ){
+            override fun getParams(): MutableMap<String, String>? {
+                val parametros = HashMap<String,String>()
+                parametros.put("idMascota",idMascota!!)
+                parametros.put("nombre",txtNombreMascotaEditar?.text.toString())
+                parametros.put("tipoMascota",txtTipoMascotaEditar?.text.toString())
+                parametros.put("descripcion",txtDescripcionMascotaEditar?.text.toString())
+                parametros.put("idUsuario",idUsuario!!)
+                return parametros
+            }
+        }
+        queue.add(resultadoPost)
     }
 }
