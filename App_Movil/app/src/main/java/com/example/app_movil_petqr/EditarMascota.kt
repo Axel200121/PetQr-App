d3c7b3c7981134acf744d8843547f693c6de911a
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-25 22:13:19 -0500

    usando jsonObjectRequest en editar mascota
    se uso el jsonObjectRequest para recibir y pasar los campos del json a las cajas de textos

--- App_Movil/app/src/main/java/com/example/app_movil_petqr/EditarMascota.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/EditarMascota.kt
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 
 class EditarMascota : AppCompatActivity() {
     var txtNombreMascotaEditar: EditText?=null
     var txtTipoMascotaEditar: EditText?=null
     var txtDescripcionMascotaEditar: EditText?=null
     var tvIdMascotaEditar: TextView?=null
-    var idUsuario:String?=null
+    var idMascota:String?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editar_mascota)
@@ -19,7 +24,21 @@ class EditarMascota : AppCompatActivity() {
         txtDescripcionMascotaEditar= findViewById(R.id.txtDescripcionMascotaEditar)
         tvIdMascotaEditar=findViewById(R.id.tvIdMascotaEditar)
 
-        idUsuario=intent.getStringExtra("idMascota").toString()
-        tvIdMascotaEditar?.setText(idUsuario)
+        idMascota=intent.getStringExtra("idMascota").toString()
+        tvIdMascotaEditar?.setText(idMascota)
+
+        val queue= Volley.newRequestQueue(this)
+        val url="http://192.168.8.101/PetQr-App/ApiRest/mascotas/MascotaConsultar.php?idMascota=$idMascota"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,url,null,
+            Response.Listener { response ->
+                txtNombreMascotaEditar?.setText(response.getString("nombre"))
+                txtTipoMascotaEditar?.setText(response.getString("tipoMascota"))
+                txtDescripcionMascotaEditar?.setText(response.getString("descripcion"))
+            }, Response.ErrorListener { error ->
+                Toast.makeText(this,error.toString(), Toast.LENGTH_LONG).show()
+            }
+        )
+        queue.add(jsonObjectRequest)
     }
 }
