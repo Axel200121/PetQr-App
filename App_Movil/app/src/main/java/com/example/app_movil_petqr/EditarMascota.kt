fc7bcb997d55b7267fe7a2851a81769bcd2d6659
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-25 22:05:40 -0500

    Recibiendo el idMascoat
    se recupero el id de la mascota y se incializo los texviews para ser utlizados

--- App_Movil/app/src/main/java/com/example/app_movil_petqr/EditarMascota.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/EditarMascota.kt
@@ -2,10 +2,24 @@ package com.example.app_movil_petqr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class EditarMascota : AppCompatActivity() {
+    var txtNombreMascotaEditar: EditText?=null
+    var txtTipoMascotaEditar: EditText?=null
+    var txtDescripcionMascotaEditar: EditText?=null
+    var tvIdMascotaEditar: TextView?=null
+    var idUsuario:String?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editar_mascota)
+        txtNombreMascotaEditar = findViewById(R.id.txtNombreMascotaEditar)
+        txtTipoMascotaEditar = findViewById(R.id.txtTipoMascotaEditar)
+        txtDescripcionMascotaEditar= findViewById(R.id.txtDescripcionMascotaEditar)
+        tvIdMascotaEditar=findViewById(R.id.tvIdMascotaEditar)
+
+        idUsuario=intent.getStringExtra("idMascota").toString()
+        tvIdMascotaEditar?.setText(idUsuario)
     }
 }
