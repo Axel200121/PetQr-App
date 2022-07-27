2a55b0514c91e168c0e6afbdec5668db570ede3a
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-27 08:28:40 -0500

    Devolviendo valores del QR leido

--- App_Movil/app/src/main/java/com/example/app_movil_petqr/LectorQR.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/LectorQR.kt
@@ -1,8 +1,10 @@
 package com.example.app_movil_petqr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 
 class LectorQR : AppCompatActivity() {
@@ -18,5 +20,18 @@ class LectorQR : AppCompatActivity() {
 
     fun iniciarScanner(){
         IntentIntegrator(this).initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+        if (result != null){
+            if (result.contents==null){
+                Toast.makeText(this,"Cancelado",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this,"El valor escaneado es: ${result?.contents}",Toast.LENGTH_LONG).show()
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
