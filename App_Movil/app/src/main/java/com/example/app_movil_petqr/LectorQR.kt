ef905721f4af0b743b30b2f8303dcaeab7098ac2
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-27 08:18:56 -0500

    Llamando el lector qr de la libreria zxing

--- App_Movil/app/src/main/java/com/example/app_movil_petqr/LectorQR.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/LectorQR.kt
@@ -2,10 +2,21 @@ package com.example.app_movil_petqr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.zxing.integration.android.IntentIntegrator
 
 class LectorQR : AppCompatActivity() {
+    var btnLectorQR:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lector_qr)
+        btnLectorQR=findViewById(R.id.btnLectroQR)
+        btnLectorQR?.setOnClickListener {
+            iniciarScanner()
+        }
+    }
+
+    fun iniciarScanner(){
+        IntentIntegrator(this).initiateScan()
     }
 }
