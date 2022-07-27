dd05616065ef311808920588d789d18c5782632f
Axel Martinez Sanjuan <85527179+Axel200121@users.noreply.github.com>
2022-07-27 08:54:15 -0500

    Almacenando la infromacion en un Texview
    Se paso la información recuperada del lector qr a un texview para ser mostrado

--- App_Movil/app/src/main/java/com/example/app_movil_petqr/LectorQR.kt
+++ App_Movil/app/src/main/java/com/example/app_movil_petqr/LectorQR.kt
@@ -4,15 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 
 class LectorQR : AppCompatActivity() {
     var btnLectorQR:Button?=null
+    var tvValorQR:TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lector_qr)
         btnLectorQR=findViewById(R.id.btnLectroQR)
+        tvValorQR=findViewById(R.id.tvValorQR)
         btnLectorQR?.setOnClickListener {
             iniciarScanner()
         }
@@ -31,7 +34,7 @@ class LectorQR : AppCompatActivity() {
             if (result.contents==null){
                 Toast.makeText(this,"Cancelado",Toast.LENGTH_LONG).show()
             }else{
-                Toast.makeText(this,"El valor escaneado es: ${result?.contents}",Toast.LENGTH_LONG).show()
+                tvValorQR?.setText(result.contents)
             }
         }else{
             super.onActivityResult(requestCode, resultCode, data)
